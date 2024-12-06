c071a002a03b7a88ebd5468b35196268ba761be3
Byron Keet <keetbis@gmail.com>
2024-12-06 10:30:49 +0200

    updated tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
+import defaultTheme from '@tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       typography: {
